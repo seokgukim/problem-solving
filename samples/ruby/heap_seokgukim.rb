34ef83e0752eb83a90690f41e6ef16c81ce0c43a
seokgukim <rokja97@naver.com>
2025-05-17 02:24:24 +0900

    feat: ruby heap
    - 루비 힙 구현

---
+++ samples/ruby/heap_seokgukim.rb
@@ -0,0 +1,65 @@
+class Heap
+    def initialize
+     	@heap = []
+  	end
+
+	def insert(value)
+		@heap << value
+		bubble_up(@heap.size - 1)
+	end
+
+	def pop
+		return nil if @heap.empty?
+		root = @heap[0]
+		swap(0, @heap.size - 1)
+		@heap.pop
+		bubble_down(0)
+		return root
+	end
+
+	def peek
+		return nil if @heap.empty?
+		return @heap[0]
+	end
+
+	def size
+		return @heap.size
+	end
+
+	def empty?
+		return @heap.empty?
+	end
+
+	private
+
+	def heapify_up(index)
+		parent_index = (index - 1) / 2
+		return if index == 0 || @heap[index] <= @heap[parent_index]
+
+		swap(index, parent_index)
+		bubble_up(parent_index)
+	end
+
+	def heapify_down(index)
+		left_child_index = index * 2 + 1
+		right_child_index = index * 2 + 2
+		largest_index = index
+
+		if left_child_index < @heap.size && @heap[left_child_index] > @heap[largest_index]
+		largest_index = left_child_index
+		end
+
+		if right_child_index < @heap.size && @heap[right_child_index] > @heap[largest_index]
+		largest_index = right_child_index
+		end
+
+		return if largest_index == index
+
+		swap(index, largest_index)
+		heapify_down(largest_index)
+	end
+
+	def swap(index1, index2)
+		@heap[index1], @heap[index2] = @heap[index2], @heap[index1]
+	end
+end
