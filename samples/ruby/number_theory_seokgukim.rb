6c405aaec5601f5ac3a65b57999e536195a8d08c
seokgukim <rokja97@naver.com>
2025-08-27 20:14:29 +0900

    feat: add extended GCD and general CRT implementations

--- samples/ruby/number_theory_seokgukim.rb
+++ samples/ruby/number_theory_seokgukim.rb
@@ -20,3 +20,38 @@ def crt(congruences)
   sum = congruences.reduce(0) { |acc, (rem, mod)| acc + rem * (prod / mod) * mul_inv(prod / mod, mod) }
   sum % prod
 end
+
+def extended_gcd(a, b)
+  # Extended Euclidean Algorithm (non-recursive)
+  # Returns [gcd, x, y] where gcd = a*x + b*y
+  
+  prev_r, r = a, b
+  prev_s, s = 1, 0
+  prev_t, t = 0, 1
+  
+  while r != 0
+    quotient = prev_r / r
+    prev_r, r = r, prev_r - quotient * r
+    prev_s, s = s, prev_s - quotient * s
+    prev_t, t = t, prev_t - quotient * t
+  end
+
+  [prev_r, prev_s, prev_t]
+end
+
+def crt_general(congruences)
+  # General Chinese Remainder Theorem implementation
+  # Returns [x, m] where x is the solution and m is the modulus
+
+  a0 = congruences[0][0]; m0 = congruences[0][1]
+  congruences[1..].each do |(a, m)|
+    g = m0.gcd(m)
+    return [-1, -1] if (a0 - a) % g != 0
+    gg, pp, qq = extended_gcd(m0 / g, m)
+    mod = m0.lcm(m)
+    x = (a0 * (m / g) * qq + a * (m0 / g) * pp) % mod
+    x += mod if x < 0
+    a0, m0 = x, mod
+  end
+  [a0 % m0, m0]
+end
